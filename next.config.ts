399efbd0188dea01c9d8e438976707b24b1a36d0
EgorUU <egorsai@gmail.com>
2025-04-20 17:55:03 +0300

    second

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '', // замените your-repo-name на название вашего репозитория
-  basePath: "/Currency-Converter",
 };
 
 
